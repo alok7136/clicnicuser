5ab8ea587aad7a8f948cecee26518e729a1fa5ae
alok kumar chaurasia <96462539+alok7136@users.noreply.github.com>
2021-12-22 16:47:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,18 +7,30 @@ import Login from "./login";
 import { Link ,BrowserRouter,Route,Routes} from "react-router-dom";
 export default class App extends react.Component
 {
-render(){
+  constructor() 
+  {
+  super()
+  this.state= {
+   menu:true,
+  }
+  }
+  changemenu=(a)=>{
+    this.setState({menu:a})
+  }
+  render(){
 return <div>
   <BrowserRouter>
-  {/* <Link to="/home"><b>Add Patient</b></Link>&nbsp;&nbsp;&nbsp; */}
-  {/* <Link to="/view"><b>View Patient</b></Link> */}
-  {/* <Link to="/login"><b>login</b></Link> */}
-  <h4 className="menu">&nbsp;&nbsp;<Link to="/registration"><b>Master Registration</b></Link>&nbsp;&nbsp;</h4>
+  {this.state.menu?<Link to="/login"><b>login</b></Link>:<div>
+  <Link to="/home"><b>Add Patient</b></Link>&nbsp;&nbsp;&nbsp;
+  <Link to="/view"><b>View Patient</b></Link>&nbsp;&nbsp;&nbsp;
+  <Link to="/login" onClick={()=>{this.setState({menu:true})}}><b>logout</b></Link>
+    </div>}
+  {/* <h4 className="menu">&nbsp;&nbsp;<Link to="/registration"><b>Master Registration</b></Link>&nbsp;&nbsp;</h4> */}
   <Routes>
-        {/* <Route exect path="/home" element={<Patient/>}/> */}
-        {/* <Route exect path="/view" element={<ViewPatient/>}/> */}
+        <Route exect path="/home" element={<Patient/>}/>
+        <Route exect path="/view" element={<ViewPatient/>}/>
         <Route exect path="/registration" element={<Registration/>}/>
-        <Route exect path="/login" element={<Login/>}/>
+        <Route exect path="/login" element={<Login changemenu={this.changemenu}/>}/>
   </Routes>
   </BrowserRouter>
   </div>
